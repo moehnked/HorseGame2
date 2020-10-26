d63c9a0f2b68e2a4faf1702e04bd6764cfbefaae
moehnked <40707466+moehnked@users.noreply.github.com>
2020-10-25 17:35:54 -0700

    basketball complete

--- Scripts/Items/Basketball.gd
+++ Scripts/Items/Basketball.gd
@@ -1,12 +1,11 @@
 extends "res://Scripts/Item.gd"
-var is_takeable = false
 var parent_transform = null
 var playerRef = null
-
+export var launch_power = 100
 
 func _process(delta):
 	if parent_transform != null:
-		global_transform.origin = parent_transform.global_transform.origin
+		owner.global_transform.origin = parent_transform.global_transform.origin
 		
 		if Input.is_action_just_released("standard"):
 			shoot_basket()
@@ -16,22 +15,22 @@ func disable_collisions():
 
 func enable_collisions():
 	owner.set_collision_mask_bit(2, 1)
+	is_interactable = true
 
 func interact(controller):
-	if(!is_takeable):
-		print("HAHAHHAHA BASKETBALL")
-		controller.inventory.append(self)
-		is_interactable = false
-		controller.clear()
-		playerRef = controller.owner
-		parent_transform = playerRef.get_palm()
-		playerRef.revoke_casting()
-		playerRef.revoke_menu_options()
-		disable_collisions()
+	print("HAHAHHAHA BASKETBALL")
+	controller.inventory.append(self)
+	is_interactable = false
+	controller.clear()
+	playerRef = controller.owner
+	parent_transform = playerRef.get_palm()
+	playerRef.revoke_casting()
+	playerRef.revoke_menu_options()
+	disable_collisions()
 
 
 func shoot_basket():
-	var dir = 100 * (parent_transform.global_transform.origin - playerRef.global_transform.origin)
+	var dir = launch_power * (parent_transform.global_transform.origin - playerRef.global_transform.origin)
 	owner.apply_central_impulse(dir)
 	parent_transform = null
 	playerRef.restore_menu_options()
