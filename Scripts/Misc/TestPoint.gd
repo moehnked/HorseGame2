8e7065ebc2cb69765dc752e0717da8283106fa4f
moehnked <moehnked@oregonstate.edu>
2020-11-17 02:24:48 -0800

    test point on ground
    test point will drop to ground after spawn

--- Scripts/Misc/TestPoint.gd
+++ Scripts/Misc/TestPoint.gd
@@ -8,9 +8,12 @@ extends Spatial
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	print("HERE I AM ", global_transform.origin)
 	pass # Replace with function body.
+
+func _process(delta):
+	if(not $RayCast.is_colliding()):
+		global_transform.origin.y -= 0.1
 
 func is_test_point():
 	return true
