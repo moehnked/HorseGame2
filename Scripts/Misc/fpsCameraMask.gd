5ca1a5b765be0ee87937e457c84fe2f7d047684b
moehnked <moehnked@oregonstate.edu>
2020-11-21 23:30:32 -0800

    update

--- Scripts/Misc/fpsCameraMask.gd
+++ Scripts/Misc/fpsCameraMask.gd
@@ -4,7 +4,7 @@ func get_head():
 	return owner.get_head()
 
 func start_swimming():
-	get_head().set_mask(Color(0, 0.80, 0.93, 0.32))
+	get_head().set_mask(Color(0, 0.70, 0.98, 0.32))
 
 func stop_swimming():
 	get_head().set_mask(Color(1,1,1,0))
